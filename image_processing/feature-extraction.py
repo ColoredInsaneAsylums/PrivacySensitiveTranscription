f744b0e10db81b3a7d0780ee989fe169257555a7
Brandon Dang <frogninja@sbcglobal.net>
2017-12-18 15:37:21 -0600

    help text

--- image_processing/feature-extraction.py
+++ image_processing/feature-extraction.py
@@ -17,11 +17,11 @@ def main():
     # load images in black/white
     ims = {filename: cv2.imread(path + filename, 0) for filename in os.listdir(path)}
 
-    # find largest image dimensions
+    # find largest image dimensions and align to block size and stride
+    print('[INFO] Calculating image window size...')
     height = max([im.shape[0] for im in ims.values()])
     width = max([im.shape[1] for im in ims.values()])
 
-    # align image dimensions to block size and stride
     height += 8 - (height % 8)
     width += 8 - (width % 8)
 
@@ -33,6 +33,7 @@ def main():
                             _nbins = 9)
 
     # evaluate image files
+    print('[INFO] Reshaping images and computing HOG features...')
     for filename, im in ims.items():
         # resize image and compute features
         im = cv2.resize(im, (width, height))
@@ -42,15 +43,14 @@ def main():
         h_features.append(h)
 
     # save data
+    print('[INFO] Saving HOG features and corresponding image name to \'features.pickle\'.')
     with open('features.pickle', 'wb') as handle:
         pickle.dump(features, handle)
 
+    print('[INFO] Saving HOG feature vectors to \'hog_features.csv\'.')
     numpy.savetxt('hog_features.csv',
                   numpy.array(h_features),
                   delimiter=',')
-
-    print('[INFO] HOG features and corresponding image name saved to \'features.pickle\'.')
-    print('[INFO] HOG feature vectors saved to \'hog_features.csv\'.')
 
 if __name__ == '__main__':
     main()
