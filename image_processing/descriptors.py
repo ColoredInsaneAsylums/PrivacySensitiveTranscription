a5043e902fdf79247f36e2420f1298dc43bea79d
Brandon Dang <frogninja@sbcglobal.net>
2017-12-21 20:55:54 -0600

    small stuff

--- image_processing/descriptors.py
+++ image_processing/descriptors.py
@@ -6,12 +6,11 @@ class SIFT:
 
     # initialize SIFT descriptor
     def __init__(self):
-        self.descriptor = cv2.xfeatures2d.SIFT_create() 
-        self.name = 'SIFT'
+        self.descriptor = cv2.xfeatures2d.SIFT_create()
 
     # compute SIFT descriptors
     def compute(self, im):
-        return self.descriptor.detectAndCompute(im, None)[1]
+        return self.descriptor.detectAndCompute(im, None)[1].flatten()
 
 # Speeded-Up Robust Features
 class SURF:
@@ -19,11 +18,10 @@ class SURF:
     # initialize SURF descriptor
     def __init__(self, hessianThreshold=400):
         self.descriptor = cv2.xfeatures2d.SURF_create(hessianThreshold)
-        self.name = 'SURF'
 
     # compute SURF descriptors
     def compute(self, im):
-        return self.descriptor.detectAndCompute(im, None)[1]
+        return self.descriptor.detectAndCompute(im, None)[1].flatten()
 
 # Histogram of Oriented Gradients
 class HOG:
@@ -35,7 +33,6 @@ class HOG:
                                             _blockStride = (8,8),
                                             _cellSize = (8,8),
                                             _nbins = 9)
-        self.name = 'HOG'
 
     # compute HOG descriptors
     def compute(self, im):
@@ -47,9 +44,8 @@ class ORB:
     # initialize ORB descriptor
     def __init__(self):
         self.descriptor = cv2.ORB_create()
-        self.name = 'ORB'
 
     # compute ORB descriptors
     def compute(self, im):
-        return self.descriptor.compute(im, None)[1]
+        return self.descriptor.compute(im, None)[1].flatten()
 
