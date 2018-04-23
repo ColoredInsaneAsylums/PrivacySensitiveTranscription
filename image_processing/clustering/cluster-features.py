a271463a1a2ecb65837671726135b4142d5b423a
budang <frogninja@sbcglobal.net>
2018-04-23 15:43:26 +0000

    bugfixes

--- image_processing/clustering/cluster-features.py
+++ image_processing/clustering/cluster-features.py
@@ -5,8 +5,8 @@ import _pickle as pickle
 from clusterers import DBSCAN, HDBSCAN
 
 # train a density-based model using the feature vectors
-def main(feats_path, clstr_algo, output_name):
-    print('[INFO] Preparing to cluster features from \'' + feats_path + '\'')
+def main(feats_path, clstr_algo):
+    print('[INFO] Preparing to cluster features from ' + feats_path)
 
     # load feature vectors
     with open(feats_path, 'rb') as handle:
@@ -24,14 +24,16 @@ def main(feats_path, clstr_algo, output_name):
     elif clstr_algo == 'hdbscan':
         clusterer = HDBSCAN()
 
-    # cluster and persist model
+    # cluster and persist labels
     clusterer.fit(dataset)
-    clusterer.save(index.keys(), output_name)
+    output = '../labels/hdbscan_' + feats_path.split('.pickle')[0].split('/')[-1] + '.pickle'
+    with open(output, 'wb') as handle:
+        pickle.dump(dict(zip(index.keys(), clusterer.labels)), handle, protocol=4)
 
 if __name__ == '__main__':
     # require filepath of features and name of clusterer to use
     parser = argparse.ArgumentParser(description='Extract image feature vectors using feature descriptors')
-    parser.add_argument('-p', '--path', required=True,
+    parser.add_argument('-f', '--feats', required=True,
                         nargs='?', action='store', const='./features/hog_features.pickle',
                         type=str, dest='feats_path',
                         help='The filepath of the feature vectors')
@@ -40,15 +42,10 @@ if __name__ == '__main__':
                         nargs='?', action='store', const='hdbscan',
                         type=str, dest='clstr_algo',
                         help='The clustering algorithm to use')
-    parser.add_argument('-o', '--output_name', required=True,
-                        nargs='?', action='store',
-                        type=str, dest='output_name',
-                        help='The name which to save the output')
 
     args = vars(parser.parse_args())
     feats_path = args['feats_path']
     clstr_algo = args['clstr_algo']
-    output_name = args['output_name']
 
-    main(feats_path, clstr_algo, output_name)
+    main(feats_path, clstr_algo)
 
